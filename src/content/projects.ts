35435ed730279b6b007e91ca8b074b2596dc8aa8
RobelAraia <robelaraia@hotmail.com>
2023-09-14 17:42:20 +0100

    changed text for project

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -12,7 +12,7 @@ export default [
   {
     title: "Robel Araia's Portfolio Website",
     description:
-      "My portfolio website serves as an online showcase of my skills, projects, and achievements in the field of web development. It's a dynamic platform where I present my professional profile and provide details about my work experience, projects, and qualifications.",
+      "My portfolio website serves as an online showcase of my skills, projects, and achievements. It's a dynamic platform where I present my professional profile and provide details about my projects, and qualifications.",
     skills: [
       "Web Development",
       "Version Control",
